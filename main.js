c36fc729c8e609026c6585b4c5676b70623c0cb6
TommyX12 <tommyx058@gmail.com>
2017-11-01 14:07:02 -0400

    Tried jquery load()

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 var $div_main;
 var $div_main_content;
 
-function main(){
-	$div_main = $('.main');
-	$div_main_content = $('.main-content');
-	$div_main.hide();
-	$div_main_content.hide();
-	
-	$div_main.fadeIn(500);
-	$div_main_content.slideDown(500);
+function ready(){
+    
+    $('#header').load('components/header/header.html')
+    $('#footer').load('components/footer/footer.html')
+    
+    
+    /* $div_main = $('.main');
+    $div_main_content = $('.main-content');
+    $div_main.hide();
+    $div_main_content.hide();
+    
+    $div_main.fadeIn(500);
+    $div_main_content.slideDown(500); */
 }
 
-
-$(document).ready(main);
+$(document).ready(ready);
